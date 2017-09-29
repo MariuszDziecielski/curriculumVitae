97cb5122d64fe499394ba0cdf54414f642a3492f
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-09-29 07:14:13 +0200

    Change occupational experience section & add minor patch in rwd

---
+++ js/script.js
@@ -0,0 +1,4 @@
+if ($(window).width() <= 360) {
+    $(".skills h2").html('<i class="fa fa-code" aria-hidden="true"></i>Umiejętności');
+    $(".social_media .portfolio a").text("https://mariuszdziecielski.github.io/...");
+}
