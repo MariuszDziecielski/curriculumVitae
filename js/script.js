22c408df31d6171f62580cbeec48afb8d8eeaea5
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-11-15 07:57:23 +0100

    Remove a photo and a few projects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 if ($(window).width() <= 360) {
-    $(".skills h2").html('<i class="fa fa-code" aria-hidden="true"></i>Umiejętności');
     $(".social_media .portfolio a").text("https://mariuszdziecielski.github.io/...");
+    $(".projects h2").html('<i class="fa fa-laptop" aria-hidden="true"></i>Projekty');
+    $(".skills h2").html('<i class="fa fa-code" aria-hidden="true"></i>Umiejętności');
 }
