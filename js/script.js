6f8c743ee94a9e26a707e4156c951a5b5152c9c4
Mariusz Dziecielski <mariuszdziec@gmail.com>
2018-03-11 18:39:48 +0100

    Add link to LinkedIn profile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 if ($(window).width() <= 360) {
     $(".social_media .portfolio a").text("https://mariuszdziecielski.github.io/...");
+    $(".social_media .linkedin a").text("https://www.linkedin.com/in/...");
     $(".projects h2").html('<i class="fa fa-laptop" aria-hidden="true"></i>Projekty');
     $(".skills h2").html('<i class="fa fa-code" aria-hidden="true"></i>Umiejętności');
 }
